ed6b314eff490398b7cf11647d33f2e42500ac37
Endeavour <martin.wonders@unn.ac.uk>
2024-10-17 20:02:39 +0100

    Part 2 Custom Widgets - Exercise 2 (rectangle and line)

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -13,7 +13,21 @@ class CircleWidget(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.drawEllipse(50,50,200,200)
 
+class ShapeWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Draw Shapes")
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        #draw a rectangle
+        painter.drawRect(50,50,150,100)
+        painter.setRenderHint(QPainter.Antialiasing)
+        #draw a line
+        painter.drawLine(50, 200, 200, 250)
+
+
 app = QApplication(sys.argv)
-window = CircleWidget()
+window = ShapeWidget()
 window.show()
 sys.exit(app.exec())
