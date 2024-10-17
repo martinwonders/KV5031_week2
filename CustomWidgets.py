b1d7fdea79b7be44fec5328fcfee2e5f4a122f48
Endeavour <martin.wonders@unn.ac.uk>
2024-10-17 20:20:26 +0100

    Part 2 Custom Widgets - Exercise 4 (different pens and brushes)

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtGui import QPainter, QBrush, QColor, QPen
+from PySide6.QtGui import (QPainter, QBrush, QColor, QPen)
+from PySide6.QtCore import Qt
 
 
 class CircleWidget(QWidget):
@@ -50,8 +51,40 @@ class CustomShapeWidget(QWidget):
         painter.drawEllipse(250, 50, 100,100)
 
 
+class MultipleShapesWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Multiple Shapes")
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        #set the pen and brush for the first shape
+        pen = QPen(Qt.DashLine)
+        pen.setWidth(2)
+        brush = QBrush(Qt.SolidPattern)
+        painter.setBrush(brush)
+        painter.setPen(pen)
+
+        #Draw a rectangle
+        painter.drawRect(50,50,100,100)
+
+        #Change the pen and brush
+        pen.setStyle(Qt.DotLine)
+        brush.setColor(Qt.yellow)
+        painter.setBrush(brush)
+        painter.setPen(pen)
+
+        painter.drawEllipse(200, 50, 100,100)
+
+        #Draw a line with a thicker pen
+        pen.setWidth(5)
+        painter.setPen(pen)
+        painter.drawLine(50, 200, 300, 200)
+
+
 
 app = QApplication(sys.argv)
-window = CustomShapeWidget()
+window = MultipleShapesWidget()
 window.show()
 sys.exit(app.exec())
